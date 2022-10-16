02585b4ae6f70f62926544784fc3b3249122dd82
Miqdad <miqdaddeveloper@gmail.com>
2022-10-16 16:44:55 +0700

    Add constant

---
+++ lib/utils/constant.dart
@@ -0,0 +1,198 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle textSplash = GoogleFonts.ubuntu(
+    color: Colors.grey,
+    fontSize: 20,
+    fontWeight: FontWeight.w500
+);
+
+TextStyle textOnBoardingHead = GoogleFonts.poppins(
+    color: Colors.black,
+    fontSize: 30,
+    fontWeight: FontWeight.w500
+);
+
+TextStyle textOnBoardingSub = GoogleFonts.poppins(
+    color: Colors.grey,
+    fontSize: 16,
+    fontWeight: FontWeight.w400
+);
+
+TextStyle textButtonBoarding = GoogleFonts.poppins(
+    color: Colors.white,
+    fontSize: 16,
+    fontWeight: FontWeight.w500
+);
+
+TextStyle textButtonSkipBoarding = GoogleFonts.poppins(
+    color: orange1,
+    fontSize: 13,
+    fontWeight: FontWeight.w400
+);
+
+TextStyle textSignUp = GoogleFonts.poppins(
+    color: Colors.black,
+    fontSize: 16,
+    fontWeight: FontWeight.w600
+);
+
+TextStyle textName = GoogleFonts.poppins(
+    color: Colors.black,
+    fontSize: 16,
+    fontWeight: FontWeight.w600
+);
+
+TextStyle textDate = GoogleFonts.poppins(
+    color: darkGrey1,
+    fontSize: 14,
+    fontWeight: FontWeight.w600
+);
+
+
+TextStyle textMyBalance = GoogleFonts.poppins(
+    color: Colors.white,
+    fontSize: 12,
+    fontWeight: FontWeight.w400
+);
+
+TextStyle featuresText = GoogleFonts.poppins(
+    color: Colors.black,
+    fontSize: 14,
+    fontWeight: FontWeight.w500
+);
+
+TextStyle categoryText = GoogleFonts.roboto(
+    color: Colors.black,
+    fontSize: 19,
+    fontWeight: FontWeight.w600
+);
+
+TextStyle myLocText = GoogleFonts.poppins(
+    color: Colors.grey,
+    fontSize: 12,
+    fontWeight: FontWeight.w500
+);
+
+
+TextStyle locationText = GoogleFonts.roboto(
+    color: Colors.black,
+    fontSize: 15,
+    fontWeight: FontWeight.w800
+);
+
+TextStyle foodNameText = GoogleFonts.roboto(
+    color: Colors.black,
+    fontSize: 14,
+    fontWeight: FontWeight.w400
+);
+
+TextStyle foodPriceListText = GoogleFonts.roboto(
+    color: red1,
+    fontSize: 15,
+    fontWeight: FontWeight.w500
+);
+
+TextStyle foodPriceText = GoogleFonts.roboto(
+    color: red1,
+    fontSize: 16,
+    fontWeight: FontWeight.w500
+);
+
+TextStyle priceCheckoutText = GoogleFonts.roboto(
+    color: grey1,
+    fontSize: 14,
+    fontWeight: FontWeight.w700
+);
+
+TextStyle foodRatingText = GoogleFonts.roboto(
+    color: Colors.black,
+    fontSize: 12,
+    fontWeight: FontWeight.w500
+);
+
+TextStyle screenInfoText = GoogleFonts.roboto(
+    color: grey1,
+    fontSize: 16,
+    fontWeight: FontWeight.w500
+);
+
+TextStyle hintSearchBarText = GoogleFonts.roboto(
+    color: Colors.grey,
+    fontSize: 15,
+    fontWeight: FontWeight.w400
+);
+
+TextStyle headDescriptionText = GoogleFonts.roboto(
+    color: Colors.grey,
+    fontSize: 14,
+    fontWeight: FontWeight.w600
+);
+
+TextStyle descriptionBarText = GoogleFonts.roboto(
+    color: darkGrey1,
+    fontSize: 14,
+    fontWeight: FontWeight.w500
+);
+
+TextStyle headPaymentText = GoogleFonts.roboto(
+    color: Colors.black,
+    fontSize: 16,
+    fontWeight: FontWeight.w600
+);
+
+TextStyle headBottomSheetText = GoogleFonts.poppins(
+    color: Colors.black,
+    fontSize: 18,
+    fontWeight: FontWeight.w600
+);
+
+TextStyle subBottomSheeetText = GoogleFonts.roboto(
+    color: grey1,
+    fontSize: 12,
+    fontWeight: FontWeight.w500
+);
+
+TextStyle subBottomSheeetText2 = GoogleFonts.poppins(
+    color: grey1,
+    fontSize: 14,
+    fontWeight: FontWeight.w500
+);
+
+
+
+///Image Constant
+var imageDajek = "assets/images/logo.png";
+var imageProfile = "assets/images/profile.png";
+var car = "assets/images/car.png";
+var bike = "assets/images/bike.png";
+var more = "assets/images/more.png";
+var favorite = "assets/images/favorite.png";
+var nearby = "assets/images/nearby.png";
+var promo = "assets/images/promo.png";
+var content1 = "assets/images/content1.png";
+var content2 = "assets/images/content2.png";
+var content3 = "assets/images/content3.png";
+var addIcon = "assets/images/add_icon.png";
+var removeIcon = "assets/images/remove_icon.png";
+var search = "assets/images/search.png";
+var ovo = "assets/images/ovo.png";
+var dana = "assets/images/dana.png";
+var gopay = "assets/images/gopay.png";
+var linkaja = "assets/images/link-aja.png";
+var pointer = "assets/images/pointer.png";
+var profil = "assets/images/profil.png";
+var point = "assets/images/point.png";
+
+///Color Constant
+const Color red1 = Color(0xffE11717);
+const Color grey1 = Color(0xffCCCCCC);
+const Color darkGrey1 = Color(0xff808080);
+const Color lineGrey = Color(0xffE0E0E0);
+const Color orange1 = Color(0xffFFA6A6);
+const Color orange2 = Color(0xffFF4E4E);
+const Color lightGrey = Color(0xffFAFAFA);
+const Color peach = Color(0xffFDF3F6);
+const Color white = Color(0xffFFFFFF);
+const Color lightPeach = Color(0xffFBE9E9);
+
